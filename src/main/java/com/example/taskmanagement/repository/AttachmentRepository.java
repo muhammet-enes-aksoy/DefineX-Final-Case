19a1685b98f0e0cc8cfeb972beb2d2c8b7de3d31
Muhammet Enes Aksoy <muhammed_enes43@hotmail.com>
2025-03-16 23:07:48 +0300

    Created Repositories

---
+++ src/main/java/com/example/taskmanagement/repository/AttachmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.taskmanagement.repository;
+
+import com.example.taskmanagement.entity.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+}
