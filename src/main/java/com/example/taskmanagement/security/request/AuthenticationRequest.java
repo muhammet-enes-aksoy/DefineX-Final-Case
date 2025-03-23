8fd1540245311da75353293c86fb409c578a71ad
Muhammet Enes Aksoy <muhammed_enes43@hotmail.com>
2025-03-23 21:50:09 +0300

    update AuthenticationRequest

--- src/main/java/com/example/taskmanagement/security/request/AuthenticationRequest.java
+++ src/main/java/com/example/taskmanagement/security/request/AuthenticationRequest.java
@@ -16,10 +16,11 @@ import org.hibernate.validator.constraints.Length;
 public class AuthenticationRequest {
     @NotBlank(message = "Username cannot be blank.")
     @Length(min = 3,max = 25,message = "Username must be between 3-25 characters.")
-    @Schema(description = "Username", example = "enes_aksoy")
+    @Schema(description = "Username", example = "enes")
     private  String username;
 
-    @NotBlank(message = "Password cannot be blank.") @Length(min = 3,max = 25,message = "Password must be between 3-25 characters.")
-    @Schema(description = "Password", example = "password123", required = false)
+    @NotBlank(message = "Password cannot be blank.")
+    @Length(min = 3,max = 25,message = "Password must be between 3-25 characters.")
+    @Schema(description = "Password", example = "123", required = false)
     private String password;
 }
